7fbf424f5a7fb558befa320b3b8cee1b8a0d6d63
张振明 <15990999891@163.com>
2020-05-23 22:53:45 +0800

    剔除无用代码

--- springboot-aspect/src/main/java/com/chuhezero/AspectApplication.java
+++ springboot-aspect/src/main/java/com/chuhezero/AspectApplication.java
@@ -2,10 +2,8 @@ package com.chuhezero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class AspectApplication {
 
     public static void main(String[] args) {
